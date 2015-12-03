f944e3b695f5f79ea995050407c8f5224db285a7
Meshine001 <mingshuai001@hotmail.com>
2015-12-03 22:11:59 +0800

    init

---
+++ LHPlatform/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LHPlatform.Startup))]
+namespace LHPlatform
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
